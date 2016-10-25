75e52316cb66bf3bf1be5dcf4d7e17bd036667e6
makelele29 <jacastillo.ugr@gmail.com>
2016-10-25 14:21:15 +0200

    Prueba correcta de mi primer Test #16

--- Test.php
+++ Test.php
@@ -10,7 +10,7 @@ class MiPrimerTest extends \PHPUnit_Framework_TestCase
     // esta función no prueba ninguna otra función por lo tanto el nombre solo describe lo que hace
     // en este caso vamos a probar que True es igual a True
     public function testParaProbarQueTrueEsTrue(){
-        $variableTrue = false;
+        $variableTrue = True;
         // primero vamos a ponerlo false para que la prueba falle
 
         // Probar que $variableTrue sea True de verdad
