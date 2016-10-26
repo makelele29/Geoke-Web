0305fcdaa955f73a6153a1cf10f40d7ee1cab0c9
makelele29 <jacastillo.ugr@gmail.com>
2016-10-26 11:06:53 +0200

    Utilizando PHP y js para una prueba #16

--- script/prueba.php
+++ script/prueba.php
@@ -3,6 +3,6 @@
 function prueba($variableTrue){
   $mivarPHP='<script type="text/javascript">var mivarJS=false;document.writeln (mivarJS);</script>';
   assert($mivarPHP==True);
-  echo "<script type='text/javascript'> alert('Your message was successfully sent.'); </script>";
+  echo $mivarPHP;
 }
 ?>
