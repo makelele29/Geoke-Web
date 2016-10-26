93eae15956666cd67de121eb65a32a19bd685dd2
makelele29 <jacastillo.ugr@gmail.com>
2016-10-26 17:06:02 +0200

    Solucionando servicio rethink #16

--- instalacion.sh
+++ instalacion.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo add-apt-repository ppa:rethinkdb/ppa -y
+sudo apt-get update -qq
+sudo apt-get install rethinkdb -y
 npm install express-generator -g
 express Geoke
 cp -r package.json Geoke/
