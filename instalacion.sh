fc9514a393bcd7bf9978c44439850bb095f4203a
makelele29 <jacastillo.ugr@gmail.com>
2016-10-26 17:29:15 +0200

    Le van a dar al resthinkbd #16

--- instalacion.sh
+++ instalacion.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install rethinkdb
+sudo apt-get install build-essential protobuf-compiler python \
+                     libprotobuf-dev libcurl4-openssl-dev \
+                     libboost-all-dev libncurses5-dev \
+                     libjemalloc-dev wget m4
+wget https://download.rethinkdb.com/dist/rethinkdb-2.3.5.tgz
+tar xf rethinkdb-2.3.5.tgz
+cd rethinkdb-2.3.5
+./configure --allow-fetch
+make
+sudo make install
 sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf
 sudo vim /etc/rethinkdb/instances.d/instance1.conf
 sudo /etc/init.d/rethinkdb restart
