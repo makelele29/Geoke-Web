779a27956f39e0aff4bc13f868d32d57bb6a31be
makelele29 <jacastillo.ugr@gmail.com>
2016-10-26 11:03:02 +0200

    Utilizando PHP y js para una prueba erronea #16

--- script/prueba.php
+++ script/prueba.php
@@ -1,7 +1,7 @@
 <?php
 
 function prueba($variableTrue){
-  $mivarPHP='<script type="text/javascript">var mivarJS=true;document.writeln (mivarJS);</script>';
+  $mivarPHP='<script type="text/javascript">var mivarJS=false;document.writeln (mivarJS);</script>';
   assert($mivarPHP==True);
   echo "<script type='text/javascript'> alert('Your message was successfully sent.'); </script>";
 }
