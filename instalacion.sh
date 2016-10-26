0feec6d1942985fefc5834a6ed5d23476bb59a31
makelele29 <jacastillo.ugr@gmail.com>
2016-10-26 18:06:03 +0200

    rallado con rethinkdb #16

--- instalacion.sh
+++ instalacion.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-sudo apt-get install build-essential protobuf-compiler python \
-                     libprotobuf-dev libcurl4-openssl-dev \
-                     libboost-all-dev libncurses5-dev \
-                     libjemalloc-dev wget m4
-wget https://download.rethinkdb.com/dist/rethinkdb-2.3.5.tgz
-tar xf rethinkdb-2.3.5.tgz
-cd rethinkdb-2.3.5
-./configure --allow-fetch
-make
-sudo make install
-sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf
-sudo vim /etc/rethinkdb/instances.d/instance1.conf
-sudo /etc/init.d/rethinkdb restart
 npm install express-generator -g
 express Geoke
 cp -r package.json Geoke/
