ce5616dade072a0ce12ec0371de3da011202f929
makelele29 <jacastillo.ugr@gmail.com>
2016-10-26 16:55:03 +0200

    Cambiado de php a node.js #16

--- instalacion.sh
+++ instalacion.sh
@@ -3,8 +3,8 @@ npm install express-generator -g
 express Geoke
 cp -r package.json Geoke/
 cp -r views/* Geoke/views/
-cp -r public/*/* Geoke/public/
-cp -r test/* Geoke/
+cp -rf public/* Geoke/public/
+cp -rf test/ Geoke/
 cd Geoke/
 npm install
 npm install mocha -g
--- script/prueba.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function prueba($variableTrue){
-  $mivarPHP='<script type="text/javascript">var mivarJS=false;document.writeln (mivarJS);</script>';
-  assert($mivarPHP==True);
-  echo $mivarPHP;
-}
-?>
